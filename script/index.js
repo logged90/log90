acbf1f50268405dfb637029b1431ba42fecb56ee
logged90 <loggerwilson64@gmail.com>
2024-12-15 09:08:31 +0300

    Add files via upload

---
+++ script/index.js
@@ -0,0 +1,187 @@
+﻿/*             CEF STUDIO            */
+/*             CEF STUDIO            */
+/*             CEF STUDIO            */
+/*             CEF STUDIO            */
+/*             CEF STUDIO            */
+/*             CEF STUDIO            */
+/*             CEF STUDIO            */
+/*             CEF STUDIO            */
+/*             CEF STUDIO            */
+/*             CEF STUDIO            */
+/*             CEF STUDIO            */
+/*             CEF STUDIO            */
+/*             CEF STUDIO            */
+/*             CEF STUDIO            */
+/*             CEF STUDIO            */
+/*             CEF STUDIO            */
+/*             CEF STUDIO            */
+/*             CEF STUDIO            */
+/*             CEF STUDIO            */
+/*             CEF STUDIO            */
+/*             CEF STUDIO            */
+/*             CEF STUDIO            */
+/*             CEF STUDIO            */
+/*             CEF STUDIO            */
+/*             CEF STUDIO            */
+/*             CEF STUDIO            */
+/*             CEF STUDIO            */
+/*             CEF STUDIO            */
+/*             CEF STUDIO            */
+/*             CEF STUDIO            */
+/*             CEF STUDIO            */
+/*             CEF STUDIO            */
+/*             CEF STUDIO            */
+/*             CEF STUDIO            */
+/*             CEF STUDIO            */
+/*             CEF STUDIO            */
+/*             CEF STUDIO            */
+/*             CEF STUDIO            */
+/*             CEF STUDIO            */
+/*             CEF STUDIO            */
+/*             CEF STUDIO            */
+/*             CEF STUDIO            */
+/*             CEF STUDIO            */
+/*             CEF STUDIO            */
+/*             CEF STUDIO            */
+/*             CEF STUDIO            */
+/*             CEF STUDIO            */
+/*             CEF STUDIO            */
+/*             CEF STUDIO            */
+/*             CEF STUDIO            */
+/*             CEF STUDIO            */
+function _0x3274(_0x3bc7ff,_0x3c7b25){const _0x394040=_0x3940();return _0x3274=function(_0x32748d,_0x38601f){_0x32748d=_0x32748d-0x7e;let _0x4c6aea=_0x394040[_0x32748d];return _0x4c6aea;},_0x3274(_0x3bc7ff,_0x3c7b25);}function _0x3940(){const _0xa5bb41=['money','updateArmour','max_ammo','images/hud/wanted/0.png','server','2363524tBjCTT','1546020LEQkcO','1583370bJBRJJ','hud:update','images/hud/weapon/','slice','getAttribute','strokeDashoffset','style','587807EQmxqa','wanted','updateHealth','8zmNRPz','getElementById','game:data:pollPlayerStats','2WlHHDn','health','length','undefined','4311375IEGxNj','182ufQCYQ','238590zEjjog','active','emit','playerId','.png','armour','online','299042CdUKTr','game:data:playerStats','interface','.interface-hud','round','570PGOGjw'];_0x3940=function(){return _0xa5bb41;};return _0x3940();}const _0x5ee53b=_0x3274;(function(_0xd4c6cd,_0x3e701a){const _0x144daf=_0x3274,_0x401ea0=_0xd4c6cd();while(!![]){try{const _0x487535=parseInt(_0x144daf(0x98))/0x1*(-parseInt(_0x144daf(0x7e))/0x2)+parseInt(_0x144daf(0x8b))/0x3+-parseInt(_0x144daf(0x89))/0x4+-parseInt(_0x144daf(0x9c))/0x5+parseInt(_0x144daf(0x9e))/0x6*(-parseInt(_0x144daf(0x9d))/0x7)+parseInt(_0x144daf(0x95))/0x8*(-parseInt(_0x144daf(0x8a))/0x9)+-parseInt(_0x144daf(0x83))/0xa*(-parseInt(_0x144daf(0x92))/0xb);if(_0x487535===_0x3e701a)break;else _0x401ea0['push'](_0x401ea0['shift']());}catch(_0x625567){_0x401ea0['push'](_0x401ea0['shift']());}}}(_0x3940,0x96597));var hud=new Vue({'el':_0x5ee53b(0x81),'data':{'active':![],'server':_0x5ee53b(0x9b),'online':0x0,'playerId':0x0,'health':0x0,'armour':0x0,'wanted':_0x5ee53b(0x87),'weaponName':'','weapon':'images/hud/weapon/0.png','ammo':'','max_ammo':'','money':moneyFormat()},'mounted'(){setInterval(()=>{const _0x2a14b7=_0x3274;this[_0x2a14b7(0x94)](this[_0x2a14b7(0x99)]),this[_0x2a14b7(0x85)](this[_0x2a14b7(0xa3)]);},0x32);},'methods':{'visible'(_0x53fddd){const _0xbf435d=_0x5ee53b;if(_0x53fddd==0x0)this[_0xbf435d(0x9f)]=![];else this[_0xbf435d(0x9f)]=!![];},'updateHealth'(_0x5beb16){const _0x2252e6=_0x5ee53b,_0x3c42a8=document[_0x2252e6(0x96)](_0x2252e6(0x99)),_0x16b8ba=0x2*Math['PI']*parseFloat(_0x3c42a8[_0x2252e6(0x8f)]('r')),_0x44bcb1=_0x16b8ba-_0x5beb16/0x64*_0x16b8ba;_0x3c42a8[_0x2252e6(0x91)][_0x2252e6(0x90)]=_0x44bcb1;},'updateArmour'(_0x112e89){const _0x114016=_0x5ee53b,_0x2f32f2=document[_0x114016(0x96)](_0x114016(0xa3)),_0x1edfb0=0x2*Math['PI']*parseFloat(_0x2f32f2[_0x114016(0x8f)]('r')),_0x4c397b=_0x1edfb0-_0x112e89/0x64*_0x1edfb0;_0x2f32f2[_0x114016(0x91)]['strokeDashoffset']=_0x4c397b;}}});function moneyFormat(_0x3acecc){const _0x709059=_0x5ee53b;var _0x3beb86=''+_0x3acecc,_0x253c47=_0x3beb86,_0x5675a2=_0x3beb86[_0x709059(0x9a)],_0x58e956=0x0;for(var _0x5b6d46=0x0;_0x5b6d46<_0x5675a2-0x1;_0x5b6d46++){if(_0x253c47[_0x5b6d46]=='.')continue;(_0x5675a2-(_0x5b6d46+0x1))%0x3==0x0&&(_0x58e956++,_0x253c47=_0x253c47[_0x709059(0x8e)](0x0,_0x5b6d46+_0x58e956)+'.'+_0x253c47[_0x709059(0x8e)](_0x5b6d46+_0x58e956));}return _0x253c47;}cef['emit']('game:hud:setComponentVisible',_0x5ee53b(0x80),![]),cef[_0x5ee53b(0xa0)](_0x5ee53b(0x97),!![],0x32),cef['on'](_0x5ee53b(0x7f),(_0x1a10a0,_0x4fa6e2,_0x4bd2d3,_0x16b542,_0x4f7f67,_0x352c6a,_0x104be0,_0x248e35,_0x38b667)=>{const _0x510b6d=_0x5ee53b;hud[_0x510b6d(0x93)]='images/hud/wanted/'+_0x4f7f67+_0x510b6d(0xa2),hud['health']=Math['round'](_0x1a10a0),hud[_0x510b6d(0xa3)]=Math[_0x510b6d(0x82)](_0x4bd2d3),hud['weapon']=_0x510b6d(0x8d)+_0x352c6a+_0x510b6d(0xa2),hud['weaponName']=''+GetWeapon(_0x352c6a),hud['ammo']=_0x104be0+'/',hud[_0x510b6d(0x86)]=''+(_0x248e35-_0x104be0),hud[_0x510b6d(0x84)]=moneyFormat(_0x38b667);}),cef['on']('hud:active',_0x133562=>hud['visible'](_0x133562)),cef['on'](_0x5ee53b(0x8c),(_0x2705a3,_0x5c6f78,_0x4e70d3)=>{const _0x8c9ad9=_0x5ee53b;hud[_0x8c9ad9(0x88)]=''+_0x2705a3,hud[_0x8c9ad9(0xa4)]=_0x5c6f78,hud[_0x8c9ad9(0xa1)]=_0x4e70d3;});
+function GetWeapon(weaponid) {
+    switch (weaponid) {
+        case 1:
+            return 'Кастет';
+            break;
+        case 2:
+            return 'Клюшка';
+            break;
+        case 3:
+            return 'Дубинка';
+            break;
+        case 4:
+            return 'Нож';
+            break;
+        case 5:
+            return 'Бита';
+            break;
+        case 6:
+            return 'Лопата';
+            break;
+        case 7:
+            return 'Кий';
+            break;
+        case 8:
+            return 'Катана';
+            break;
+        case 9:
+            return 'Бензопила';
+            break;
+        case 10:
+            return 'Двухсторонний Дилдо';
+            break;
+        case 11:
+            return 'Дилдо';
+            break;
+        case 12:
+            return 'Вибратор';
+            break;
+        case 13:
+            return 'Вибратор 2';
+            break;
+        case 14:
+            return 'Букет цветов';
+            break;
+        case 15:
+            return 'Трость';
+            break;
+        case 16:
+            return 'Граната';
+            break;
+        case 17:
+            return 'Слезоточивый газ';
+            break;
+        case 18:
+            return 'Коктейль Молотова';
+            break;
+        case 22:
+            return 'Пистолет 9мм';
+            break;
+        case 23:
+            return 'Пистолет 9мм';
+            break;
+        case 24:
+            return 'Desert Eagle';
+            break;
+        case 25:
+            return 'Дробовик';
+            break;
+        case 26:
+            return 'Обрез';
+            break;
+        case 27:
+            return 'Скорострельный дробовик';
+            break;
+        case 28:
+            return 'Узи';
+            break;
+        case 29:
+            return 'МП5';
+            break;
+        case 30:
+            return 'Ak-47';
+            break;
+        case 31:
+            return 'Винтовка M4';
+            break;
+        case 32:
+            return 'Тес-9';
+            break;
+        case 33:
+            return 'Охотничье ружье';
+            break;
+        case 34:
+            return 'Снайперская винтовка';
+            break;
+        case 35:
+            return 'РПГ';
+            break;
+        case 36:
+            return 'Самонаводящиеся ракеты';
+            break;
+        case 37:
+            return 'Огнемет';
+            break;
+        case 38:
+            return 'Миниган';
+            break;
+        case 39:
+            return 'Сумка с тротилом';
+            break;
+        case 40:
+            return 'Детонатор к сумке';
+            break;
+        case 41:
+            return 'Баллончик с краской';
+            break;
+        case 42:
+            return 'Огнетушитель';
+            break;
+        case 43:
+            return 'Фотоаппарат';
+            break;
+        case 44:
+            return 'Прибор ночного видения';
+            break;
+        case 45:
+            return 'Тепловизор';
+            break;
+        case 46:
+            return 'Парашют';
+            break;
+        default:
+            return '';
+    }
+}
